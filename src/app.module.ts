5301ddf4da1b59606349e9bfe98af860e42f8589
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-05-27 11:52:15 +0500

    add users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       }),
       inject: [ConfigService]
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
